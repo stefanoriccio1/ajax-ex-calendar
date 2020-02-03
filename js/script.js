6d1d83f54db0ba0254158ff9c043ad99bcf1a461
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-03 12:02:45 +0100

    fatta chiamata Ajax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$(document).ready( function{
+$(document).ready(function(){
 
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+      method: "GET",
+      success: function(data){
+      console.log(data.response);
+      },
+      error: function(request, state, errors){
+        alert('Si è verificato un error' + errors)
+      }
+    }
+  );
 });
