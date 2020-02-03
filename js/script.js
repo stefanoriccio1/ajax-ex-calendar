d17baf08ea5f0aec4e9969a4a0b99ce640e47d21
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-03 12:14:10 +0100

    fatta funzione di printing

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
-
-  $.ajax(
+  // "response": [
+  //        {
+  //            "name": "Capodanno",
+  //            "date": "2018-01-01"
+  //        },
+  //        {
+  //            "name": "Epifania",
+  //            "date": "2018-01-06"
+  //        }
+  //    ]
+  var chiamata = $.ajax(
     {
       url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
       method: "GET",
       success: function(data){
-      console.log(data.response);
+        printData(data.response);
       },
       error: function(request, state, errors){
         alert('Si è verificato un error' + errors)
       }
     }
   );
+  function printData(festivita){
+    console.log(festivita);
+  }
 });
