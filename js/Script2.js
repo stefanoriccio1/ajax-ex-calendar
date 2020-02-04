f477dcc24c01c7cf06fc5655c95281007e052b99
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-04 15:18:57 +0100

    stampati i giorni in console

--- js/Script2.js
+++ js/Script2.js
@@ -1,1 +1,27 @@
-alert('ciao');
+$(document).ready(function(){
+
+// alert('ciao');
+
+// inizio facendo inserimento dei giorni manuale su html
+
+var currentMonth = "January";
+var days = 31;
+for (var i = 1; i <= days; i++) {
+  console.log(i);
+}
+
+// risposta Ajax:
+// {
+//     "success": true,
+//     "response": [
+//         {
+//             "name": "Capodanno",
+//             "date": "2018-01-01"
+//         },
+//         {
+//             "name": "Epifania",
+//             "date": "2018-01-06"
+//         }
+//     ]
+// }
+});
